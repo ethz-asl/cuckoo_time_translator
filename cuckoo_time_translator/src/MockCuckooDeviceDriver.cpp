db5d70bb01943966952d7a7d14590a7427988eba
HannesSommer <hannes.sommer@mavt.ethz.ch>
2018-07-26 10:00:20 +0200

    Made MockCuckooDeviceDriver more flexible and interactive.
    * support random delay simulation
    * dynamic reconfigure for some parameters

--- cuckoo_time_translator/src/MockCuckooDeviceDriver.cpp
+++ cuckoo_time_translator/src/MockCuckooDeviceDriver.cpp
@@ -1,10 +1,11 @@
 #include "MockCuckooDeviceDriver.h"
 
+#include <random>
+
 #include <cuckoo_time_translator/KalmanOwt.h>
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
-#include "ros/ros.h"
-#include "std_msgs/String.h"
+#include <ros/ros.h>
 #pragma GCC diagnostic pop
 
 namespace cuckoo_time_translator {
@@ -19,11 +20,16 @@ KalmanOwtConfig getMockKalmanConfig() {
   return kc;
 }
 
-
 MockCuckooDeviceDriver::MockCuckooDeviceDriver(ros::NodeHandle & nh) :
-  cuckooClock(kWrappingNumber, kFreq, kSkew, ros::Duration(kOffset)),
-  translator(WrappingClockParameters{kWrappingNumber, kFreq}, nh.getNamespace(), Defaults().setFilterConfig(getMockKalmanConfig()))
+  cuckooClock_(kWrappingNumber, kFreq, kSkew, ros::Duration(kOffset)),
+  translator_(WrappingClockParameters{kWrappingNumber, kFreq}, nh.getNamespace(), Defaults().setFilterConfig(getMockKalmanConfig()))
 {
+  srv_.setCallback(boost::bind(&MockCuckooDeviceDriver::dynamicReconfigureCallback, this, _1, _2));
+
+  nh.param("delay", delaySeconds_, delaySeconds_);
+  nh.param("delaySigma", delaySigmaSeconds_, delaySigmaSeconds_);
+  nh.param("assumedDelay", assumedDelaySeconds_, assumedDelaySeconds_);
+  ROS_INFO("Using initial delay=%g s, delaySigma=%g s, and assumedDelay=%g s.", delaySeconds_, delaySigmaSeconds_, assumedDelaySeconds_);
 }
 
 
@@ -31,16 +37,33 @@ void MockCuckooDeviceDriver::step() {
   ros::Time receiveTime;
   uint32_t deviceTime;
 
-  cuckooClock.getNewSimulatedMeasurementTimes(deviceTime, receiveTime);
+  static std::default_random_engine generator;
 
-  translator.update(deviceTime, receiveTime);
+  cuckooClock_.getNewSimulatedMeasurementTimes(deviceTime, receiveTime);
+
+  receiveTime += ros::Duration(delaySeconds_);
+  if(delaySigmaSeconds_){
+    std::exponential_distribution<double> delayDistribution(1/delaySigmaSeconds_);
+    receiveTime += ros::Duration(delayDistribution(generator));
+  }
+
+  translator_.update(deviceTime, receiveTime, -assumedDelaySeconds_);
 }
 } // namespace cuckoo_time_translator
 
 using namespace cuckoo_time_translator;
 
-int main(int argc, char **argv)
-{
+
+
+
+void MockCuckooDeviceDriver::dynamicReconfigureCallback(const MockCuckooClockConfig &config, int) {
+  assumedDelaySeconds_ = config.assumedDelay;
+  delaySeconds_ = config.delay;
+  delaySigmaSeconds_ = config.delaySigma;
+  ROS_INFO("Using updated delay=%g s, delaySigma=%g s, and assumedDelay=%g s.", delaySeconds_, delaySigmaSeconds_, assumedDelaySeconds_);
+}
+
+int main(int argc, char **argv) {
   ros::init(argc, argv, "mock_cuckoo_device_driver");
 
   ros::NodeHandle nh;
@@ -58,4 +81,3 @@ int main(int argc, char **argv)
 
   return 0;
 }
-
